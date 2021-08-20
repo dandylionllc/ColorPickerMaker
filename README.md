fb9b5b3e358e85c6aa21b2c2e8c290876a514142
Frank Cipolla <frankc@cscit.net>
2021-08-20 15:33:24 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ColorPickerMaker
+Referencing tutorioals:
+https://betterprogramming.pub/want-more-from-colorpicker-in-ios-14-create-1500-swiftui-pickers-of-your-own-7533514bf824
+
---
+++ Shared/ColorPickerMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerMakerApp.swift
+//  Shared
+//
+//  Created by Frank Cipolla on 8/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
